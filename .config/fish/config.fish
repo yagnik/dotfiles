358a5c7556e5c37182c7e772caaeb7c8e472b852
yagnik <yagnikkhanna@gmail.com>
2020-04-05 02:09:34 +0530

    update configs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,4 @@
 . ~/.config/fish/functions/*.fish
 . ~/.config/fish/theme/*.fish
 alias config '/usr/bin/git --git-dir=/Users/yagnik/code/dotfiles/ --work-tree=/Users/yagnik'
+set -x PATH $PATH ~/.dotfiles/bin/
