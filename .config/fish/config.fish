aca712d46d0d79483b3cd79b0424c16eb678dde5
yagnik <yagnikkhanna@gmail.com>
2021-06-08 19:59:09 +0530

    add fisher

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,5 @@
 . ~/.config/fish/functions/*.fish
 . ~/.config/fish/theme/*.fish
 alias config '/usr/bin/git --git-dir=/Users/yagnik/code/dotfiles/ --work-tree=/Users/yagnik'
-set -x PATH $PATH ~/.dotfiles/bin/ ~/code/personal/android/emulator
+
+set -x PATH $PATH ~/.dotfiles/bin/ ~/code/personal/android/emulator ~/.gem/ruby/2.6.0/bin /Applications/fish.app/Contents/Resources/base/usr/local/bin/
