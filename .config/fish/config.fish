3571f97088787d12bdf0cd1e6fa7d3505d3f4009
yagnik <yagnikkhanna@gmail.com>
2021-07-01 20:01:27 +0530

    update config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,3 +3,4 @@
 alias config '/usr/bin/git --git-dir=/Users/yagnik/code/dotfiles/ --work-tree=/Users/yagnik'
 
 set -x PATH $PATH ~/.dotfiles/bin/ ~/code/personal/android/emulator ~/.gem/ruby/2.6.0/bin /Applications/fish.app/Contents/Resources/base/usr/local/bin/
+set -gx NVM_DIR /usr/local/opt/nvm/
