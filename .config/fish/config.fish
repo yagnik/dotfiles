9ae67ab1abb09f7aa247c24119940d482bebf194
Yagnik <yagnik@users.noreply.github.com>
2022-02-05 19:51:00 +0530

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,5 @@
 . ~/.config/fish/functions/*.fish
 . ~/.config/fish/theme/*.fish
-alias config '/usr/bin/git --git-dir=/Users/yagnik/code/dotfiles/ --work-tree=/Users/yagnik'
+alias config 'git --git-dir=/Users/yagnik/code/dotfiles/ --work-tree=/Users/yagnik'
+set -x PATH $PATH ~/.dotfiles/bin/ 
 
-set -x PATH $PATH ~/.dotfiles/bin/ ~/code/personal/android/emulator ~/.gem/ruby/2.6.0/bin /Applications/fish.app/Contents/Resources/base/usr/local/bin/
-set -gx NVM_DIR /usr/local/opt/nvm/
